476ca8bb49a8b09d2fcbc899792c088de624b1b0
AtakanKorez <atakankorez@gmail.com>
2023-05-22 23:12:28 +0200

    All microservices have been upgraded to .Net 7

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,46 +1,88 @@
 using FreeCourse.Service.Catalog.Dtos;
 using FreeCourse.Service.Catalog.Services;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using FreeCourse.Service.Catalog.Settings;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using Microsoft.Extensions.Options;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
-namespace FreeCourse.Service.Catalog
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(opt =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            {
-                var host = CreateHostBuilder(args).Build();
+	opt.Filters.Add(new AuthorizeFilter());
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-                using (var scope = host.Services.CreateScope())
-                {
-                    var serviceProvider = scope.ServiceProvider;
 
-                    var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
 
-                    if (!categoryService.GetAllAsync().Result.Data.Any())
-                    {
-                        categoryService.CreateAsync(new CategoryDto { Name = "Asp.net Core Kursu" }).Wait();
-                        categoryService.CreateAsync(new CategoryDto { Name = "Asp.net Core API Kursu" }).Wait();
-                    }
-                }
+builder.Services.AddMassTransit(x =>
+{
+	// Default Port : 5672
+	x.UsingRabbitMq((context, cfg) =>
+	{
+		cfg.Host(builder.Configuration["RabbitMQUrl"], "/", host =>
+		{
+			host.Username("guest");
+			host.Password("guest");
+		});
+	});
+});
 
-                host.Run();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+	options.Authority = builder.Configuration["IdentityServerURL"];
+	options.Audience = "resource_catalog";
+	options.RequireHttpsMetadata = false;
+});
 
-            }
-        }
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICourseService, CourseService>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+	return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	var serviceProvider = scope.ServiceProvider;
+
+	var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+
+	if (!(await categoryService.GetAllAsync()).Data.Any())
+	{
+		await categoryService.CreateAsync(new CategoryDto { Name = "Asp.net Core Kursu" });
+		await categoryService.CreateAsync(new CategoryDto { Name = "Asp.net Core API Kursu" });
+	}
 }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -1,38 +1,114 @@
+using FreeCourse.Services.Order.Application.Consumers;
 using FreeCourse.Services.Order.Infrastructure;
-using Microsoft.AspNetCore.Hosting;
+using FreeCourse.Shared.Services;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
 
-namespace FreeCourse.Services.Order.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+builder.Services.AddControllers(opt =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
+	opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-            using(var scope = host.Services.CreateScope()) {
-                var serviceProvider = scope.ServiceProvider;
-                var orderDbContext = serviceProvider.GetRequiredService<OrderDbContext>();
 
-                orderDbContext.Database.Migrate();
-            }
 
-            host.Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+	options.Authority = builder.Configuration["IdentityServerURL"];
+	options.Audience = "resource_order";
+	options.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddDbContext<OrderDbContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), configure =>
+	{
+		configure.MigrationsAssembly("FreeCourse.Services.Order.Infrastructure");
+	});
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+builder.Services.AddMediatR(typeof(FreeCourse.Services.Order.Application.Handlers.CreateOrderCommandHandler).Assembly);
+
+
+
+builder.Services.AddMassTransit(x =>
+{
+
+	x.AddConsumer<CreateOrderMessageCommandConsumer>();
+	x.AddConsumer<CourseNameChangedEventConsumer>();
+	// Default Port : 5672
+	x.UsingRabbitMq((context, cfg) =>
+	{
+		cfg.Host(builder.Configuration["RabbitMQUrl"], "/", host =>
+		{
+			host.Username("guest");
+			host.Password("guest");
+		});
+
+		cfg.ReceiveEndpoint("create-order-service", e =>
+		{
+			e.ConfigureConsumer<CreateOrderMessageCommandConsumer>(context);
+		});
+		cfg.ReceiveEndpoint("course-name-changed-event-order-service", e =>
+		{
+			e.ConfigureConsumer<CourseNameChangedEventConsumer>(context);
+		});
+	});
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	var serviceProvider = scope.ServiceProvider;
+	var orderDbContext = serviceProvider.GetRequiredService<OrderDbContext>();
+	orderDbContext.Database.Migrate();
+
 }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+	app.UseDeveloperExceptionPage();
+}
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
